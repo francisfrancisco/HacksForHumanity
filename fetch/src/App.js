22575e656184fc71469e011121578bc236ec18ee
Francis Francisco <francis.francisco44@gmail.com>
2019-02-16 19:39:54 -0700

    added axios

--- fetch/src/App.js
+++ fetch/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Login from './components/login';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    axios.get(``).then(res => {
+      this.setState({: res.data})
+    })
+  }
   render() {
     return (
       <Router>
